868d8638057e320ff33af76945b8c11db441639d
Jamie Woodmancy <jamie29w@gmail.com>
2020-01-27 20:22:49 -0800

    Pulling blog posts into /writing page

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import usePosts from '../hooks/usePosts';
+
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const Writing = () => (
-  <Layout>
-    <SEO title='Writing' />
-    <h1>Hi from the writing page</h1>
-    <p>Welcome to writing</p>
-    <Link to='/'>Go back to the homepage</Link>
-  </Layout>
-);
+const Writing = () => {
+  const posts = usePosts();
+  return (
+    <Layout>
+      <SEO title='Writing' />
+      <h1>Hi from the writing page</h1>
+      <p>Welcome to writing</p>
+
+      <h2>Here are some excellent blog posts</h2>
+      {posts.map(post => (
+        <pre key={post.slug}>{JSON.stringify(post, null, 2)}</pre>
+      ))}
+    </Layout>
+  );
+};
 
 export default Writing;
