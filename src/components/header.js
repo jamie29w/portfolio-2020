a0ec7ca2a0584cb49d68cd50e1cedaf8965f9a8d
Jamie Woodmancy <jamie29w@gmail.com>
2020-02-03 17:04:06 -0800

    Created Hero.js component and added images to posts
    Added gatsby-remark-images plugin; Fixed lint issues in header.js and writing.js
    
    Added post images too

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,6 @@ const Header = ({ siteTitle }) => (
     <div
       style={{
         margin: `0 auto`,
-        padding: `0 1rem`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
         display: 'flex',
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+
+import BackgroundImage from 'gatsby-background-image';
+import styled from '@emotion/styled';
+
 import Layout from '../components/layout';
 
 export const query = graphql`
@@ -10,22 +14,47 @@ export const query = graphql`
         title
         author
         published_date
+        image {
+          sharp: childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
       }
       body
     }
   }
 `;
 
-const PostTemplate = ({ data: { mdx: post } }) => {
+const PostTemplate = ({
+  data: {
+    mdx: { frontmatter, body },
+  },
+}) => {
+  console.log('frontmatter');
+  console.log(frontmatter);
   return (
     <Layout>
-      <h1>{post.frontmatter.title}</h1>
-      <p>Posted by {post.frontmatter.author}</p>
-      <MDXRenderer>{post.body}</MDXRenderer>
-      {/* <p>Body goes here</p> */}
+      <h1>{frontmatter.title}</h1>
+      <p>Posted by {frontmatter.author}</p>
+      {frontmatter.image?.sharp?.fluid && (
+        <ImageBackground
+          tag='section'
+          fluid={frontmatter.image.sharp.fluid}
+          fadeIn='soft'
+        />
+      )}
+      <MDXRenderer>{body}</MDXRenderer>
       <Link to='/writing/'>&larr; back to other posts</Link>
     </Layout>
   );
 };
 
 export default PostTemplate;
+
+const ImageBackground = styled(BackgroundImage)`
+  background-position: center;
+  background-size: cover;
+  height: 50vh;
+`;
