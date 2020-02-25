11dde6ae1b88548daed55bdf38c988404907d868
Jamie Woodmancy <jamie29w@gmail.com>
2020-02-24 21:13:58 -0800

    Reimplemented skewed gradient on homepage; Tidied up header and footer padding logic and a typo

--- src/components/heroGradient.js
+++ src/components/heroGradient.js
@@ -1,56 +1,83 @@
 import React, { useContext } from 'react';
 import styled from '@emotion/styled';
-import { ThemeContext } from '@emotion/core';
+import { ThemeContext, css } from '@emotion/core';
+import { Link } from 'gatsby';
 
-const Hero = ({ children }) => {
-  const { padding, colors } = useContext(ThemeContext);
+const Hero = () => {
+  const { padding, colors, text } = useContext(ThemeContext);
+
+  const footerHeight = `${text.bodyFontSizeNum * 3}px`;
 
   const HeroWrapper = styled.div`
-    display: flex;
-    flex: 1;
+    position: relative;
+    height: calc(100vh - ${footerHeight});
+    background: #fff;
+  `;
+
+  const Gradient = styled.div`
+    width: 100%;
+    position: absolute;
+    top: 0;
     height: 80vh;
-    background: #fdc830; /* fallback for old browsers */
-    background: -webkit-linear-gradient(
-      #d61b11 -21%,
-      #f55b2d 36%,
-      #ffc09c 79%,
-      #fff
-    ); /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(
-      #d61b11 -21%,
-      #f55b2d 36%,
-      #ffc09c 79%,
-      #fff
-    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    overflow: hidden;
+    -webkit-transform: skewY(-12deg);
+    transform: skewY(-12deg);
+    -webkit-transform-origin: 0;
+    transform-origin: 0;
+    background: linear-gradient(150deg, #53f 15%, #05d5ff 70%, #a6ffcb 94%);
+  `;
+
+  const TextBox = styled.div`
+    position: absolute;
     display: flex;
     flex-direction: column;
     height: 100%;
+    padding: ${padding};
     width: 100%;
     z-index: 5;
 
-    h1,
-    h2,
-    h3 {
-      color: #ffffff;
-    }
-
     h1 {
-      font-size: 2.75rem;
+      color: ${colors.background};
+      font-size: 2.25rem;
     }
   `;
 
-  const ChildWrapper = styled.div`
-    height: 100%;
+  const HomePageLink = styled(Link)`
     display: flex;
+    position: absolute;
+    bottom: ${padding};
+    left: 50%;
+    transform: translateX(-50%);
     flex-direction: column;
-    justify-content: space-between;
     align-items: center;
-    padding: ${padding} ${padding} calc(8 * ${padding});
+    justify-content: center;
+    border: ${`2px solid ${colors.accentMid}`};
+    width: 245px;
+    height: 68px;
+    color: ${colors.accentMid};
+    font-size: ${text.bodyFontSize};
+    font-weight: 600;
+    text-align: center;
+    text-decoration: none;
+    z-index: 5;
   `;
 
   return (
     <HeroWrapper>
-      <ChildWrapper>{children}</ChildWrapper>
+      <TextBox>
+        <h2>jamie woodmancy</h2>
+        <div
+          css={css`
+            position: relative;
+            top: 30%;
+          `}
+        >
+          <h1>web</h1>
+          <h1>developer</h1>
+        </div>
+      </TextBox>
+      <Gradient />
+      <HomePageLink to='/writing/'>let's do this</HomePageLink>
     </HeroWrapper>
   );
 };
