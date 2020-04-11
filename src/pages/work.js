32f0db7ae73d46df5a635cf01b6379699ead2835
Jamie Woodmancy <jamie29w@gmail.com>
2020-04-11 15:48:08 -0700

    appwide - alphabetizing imports, props, declarations

--- src/pages/work.js
+++ src/pages/work.js
@@ -4,9 +4,11 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+const pageTitle = 'Work';
+
 const Work = () => (
-  <Layout title='Work'>
-    <SEO title='Work' />
+  <Layout title={pageTitle}>
+    <SEO title={pageTitle} />
     <h1>Hi from the work page</h1>
     <p>Welcome to work</p>
     <Link to='/'>Go back to the homepage</Link>
