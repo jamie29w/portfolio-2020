2af960244837bb3462f58ea8dc3def219e4e49c1
Jamie Woodmancy <jamie29w@gmail.com>
2020-05-25 14:16:34 -0700

    now page & NowSection - updated to consume now mdx files and render them into styled sections

--- src/pages/now.js
+++ src/pages/now.js
@@ -3,42 +3,21 @@ import React from 'react';
 import Layout from '../components/layout';
 import NowSection from '../components/NowSection';
 import SEO from '../components/seo';
+import useNowContent from '../hooks/useNowContent';
 
 const pageTitle = 'Now';
 
 const Now = () => {
+  const nowSections = useNowContent();
+
   return (
     <Layout title={pageTitle}>
       <SEO title={pageTitle} />
-      {nowData.map(section => (
-        <NowSection key={section.title} section={section} />
+      {nowSections.map((section, i) => (
+        <NowSection section={section} key={i} />
       ))}
     </Layout>
   );
 };
 
 export default Now;
-
-const nowData = [
-  {
-    title: 'Working with',
-    details: ['JS', 'React', 'React Native', 'Styled Components'],
-  },
-  {
-    title: 'Reading',
-    details: ['Dune Messiah', 'Harry Potter and the Prisoner of Azkaban'],
-  },
-  {
-    title: 'Listening to',
-    details: ['Syntax', 'Front End Happy Hour'],
-  },
-  {
-    title: 'What else',
-    details: [
-      'Playing cornhole with my kids',
-      'Perfecting my old fashioned',
-      'Reconsidering my opinions on React app architecture',
-      'Stumptown coffee + Chemex',
-    ],
-  },
-];
