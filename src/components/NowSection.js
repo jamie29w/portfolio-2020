5fb7630dcdf6168c5f8103297547813fcc0bc627
Jamie Woodmancy <jamie29w@gmail.com>
2020-05-25 14:16:34 -0700

    now page & NowSection - updated to consume now mdx files and render them into styled sections

--- src/components/NowSection.js
+++ src/components/NowSection.js
@@ -1,29 +1,25 @@
 import React, { useContext } from 'react';
 import styled from '@emotion/styled';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 
 import { ThemeContext } from '../providers/ThemeProvider';
 
-const NowListSection = ({ section: { title, details } }) => {
+const NowListSection = ({ section: { body } }) => {
   const {
     palette: { tertiary },
     spacing,
   } = useContext(ThemeContext);
 
   return (
-    <>
-      <h3>{title}</h3>
-      <StyledUl bordercolor={tertiary} spacing={spacing}>
-        {details.map(detailItem => (
-          <li key={detailItem}>{detailItem}</li>
-        ))}
-      </StyledUl>
-    </>
+    <Wrapper bordercolor={tertiary} spacing={spacing}>
+      <MDXRenderer>{body}</MDXRenderer>
+    </Wrapper>
   );
 };
 
 export default NowListSection;
 
-const StyledUl = styled.ul`
+const Wrapper = styled.ul`
   &:not(:last-of-type) {
     border-bottom: ${({ bordercolor }) => `2px solid ${bordercolor}`};
     padding-bottom: ${({ spacing }) => spacing}rem;
