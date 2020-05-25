941c0a269325300df4230eb36370933813778d5e
Jamie Woodmancy <jamie29w@gmail.com>
2020-05-24 21:20:44 -0700

    ThemeProvider - added user color scheme preference detection and change listener

--- src/providers/ThemeProvider.js
+++ src/providers/ThemeProvider.js
@@ -1,10 +1,33 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import createTheme from '../styles';
 
 export const ThemeContext = createContext(null);
 
 export default function ThemeProvider({ children }) {
   const [themeType, setThemeType] = useState('light');
+
+  useEffect(() => {
+    if (
+      window?.matchMedia &&
+      window?.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+      setThemeType('dark');
+    }
+
+    const handleThemePreferenceChange = e => {
+      setThemeType(e.matches ? 'dark' : 'light');
+    };
+
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', handleThemePreferenceChange);
+
+    return () => {
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .removeEventListener('change', handleThemePreferenceChange);
+    };
+  }, []);
 
   const theme = {
     ...createTheme(themeType),
