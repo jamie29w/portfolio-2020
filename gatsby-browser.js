c18510afbbe5d0d894942726c9facbfbe8e97477
Jamie Woodmancy <jamie29w@gmail.com>
2020-05-24 21:28:33 -0700

    gatsby-browser & -ssr - lint

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,7 @@
 // You can delete this file if you're not using it
 
 import React from 'react';
+
 import { GlobalComponent } from './src/styles';
 import ThemeProvider from './src/providers/ThemeProvider';
 
