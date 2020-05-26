c4140c945680bf0632462df7d1921eab9f6c0815
Jamie Woodmancy <jamie29w@gmail.com>
2020-05-25 20:49:51 -0700

    work - updated to use mdx content; theFirstLookProject.mdx - written

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
 import Layout from '../components/layout';
+import Section from '../components/Section';
 import SEO from '../components/seo';
+import useWorkContent from '../hooks/useWorkContent';
 
 const pageTitle = 'Work';
 
-const Work = () => (
-  <Layout title={pageTitle}>
-    <SEO title={pageTitle} />
-    <h3>Hi from the work page</h3>
-  </Layout>
-);
+const Work = () => {
+  const workSections = useWorkContent();
+
+  return (
+    <Layout title={pageTitle}>
+      <SEO title={pageTitle} />
+      {workSections.map((section, i) => (
+        <Section section={section} key={i} />
+      ))}
+    </Layout>
+  );
+};
 
 export default Work;
