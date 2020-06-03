9d0039df13e00db86a092a0b0929dcc1492dd938
Jamie Woodmancy <jamie29w@gmail.com>
2020-06-02 20:31:44 -0700

    got snapshot testing working - DarkModeSwitch test written

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transform: {
-    '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
+    '^.+\\.(jsx|js)?$': `<rootDir>/jest-preprocess.js`,
   },
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
--- src/components/DarkModeSwitch.test.js
+++ src/components/DarkModeSwitch.test.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import DarkModeSwitch from '.';
+import DarkModeSwitch from './DarkModeSwitch';
+import { ThemeProvider } from '../styles';
 
 describe('DarkModeSwitch', () => {
-  it('renders correctly', () => {
-    const comp = renderer.create(<DarkModeSwitch />).toJSON();
+  it('renders correctly in light mode', () => {
+    const comp = renderer
+      .create(
+        <ThemeProvider value={'light'}>
+          <DarkModeSwitch />
+        </ThemeProvider>
+      )
+      .toJSON();
+
+    expect(comp).toMatchSnapshot();
+  });
+
+  it('renders correctly in dark mode', () => {
+    const comp = renderer
+      .create(
+        <ThemeProvider value={'dark'}>
+          <DarkModeSwitch />
+        </ThemeProvider>
+      )
+      .toJSON();
 
     expect(comp).toMatchSnapshot();
   });
