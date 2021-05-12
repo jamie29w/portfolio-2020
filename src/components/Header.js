2d3de17df2a723c0b7ccc34056ce79a1653e30e5
Jamie Woodmancy <jamie.woodmancy@mutations.ltd>
2021-05-11 19:26:05 -0700

    Update Gatsby to from 2.x.x to 3.5.0
    * Update all Gatsby peer-deps
    * Fix build issue by adding .nvmrc > v14.17.0
    * Update @emotion/core to @emotion/react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { css } from '@emotion/core';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Link } from 'gatsby';
 
